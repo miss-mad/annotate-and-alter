505e9fc2f4d37f461e95a93646408a5eaa27ebb5
miss-mad <mmlh.harris@gmail.com>
2022-11-15 22:47:29 -0500

    add comments to server.js + personalize index.html starter code

--- Develop/server/server.js
+++ Develop/server/server.js
@@ -1,12 +1,19 @@
+// setting up a simple server with express
+
 const express = require('express');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+// express middleware to create routes for the dist folder inside the client side folder; serves static files in the entire client's dist folder; want to give them the bundled files, not the unbundled files
 app.use(express.static('../client/dist'));
+
+// more middleware to handle json and url encoding
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+// then connect the HTML routes to the app
 require('./routes/htmlRoutes')(app);
 
-app.listen(PORT, () => console.log(`Now listening on port: ${PORT}`));
+// tell the app which port to listen to
+app.listen(PORT, () => console.log(`Now listening on http://localhost:${PORT}`));
