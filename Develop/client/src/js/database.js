0b9a54b590a28d47e2bcc483741d64c00e384148
miss-mad <mmlh.harris@gmail.com>
2022-11-16 16:10:49 -0500

    added GET and PUT http request methods

--- Develop/client/src/js/database.js
+++ Develop/client/src/js/database.js
@@ -1,21 +1,42 @@
-import { openDB } from 'idb';
+import { openDB } from "idb";
 
 const initdb = async () =>
-  openDB('jate', 1, {
+  openDB("textEditor", 1, {
     upgrade(db) {
-      if (db.objectStoreNames.contains('jate')) {
-        console.log('jate database already exists');
+      if (db.objectStoreNames.contains("textEditor")) {
+        console.log("textEditor database already exists");
         return;
       }
-      db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
-      console.log('jate database created');
+      db.createObjectStore("textEditor", {
+        keyPath: "id",
+        autoIncrement: true,
+      });
+      console.log("textEditor database created");
     },
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  console.error("putDb not implemented");
+  const textEditorDb = await openDB("textEditor", 1);
+  const transaction = textEditorDb.transaction("textEditor", "readwrite");
+  const store = transaction.objectStore("textEditor");
+  // insert data into put request
+  const putRequest = store.put({});
+  const result = await putRequest;
+  console.log("data updated in the database", result);
+};
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  console.error("getDb not implemented");
+  const textEditorDb = await openDB("textEditor", 1);
+  const transaction = textEditorDb.transaction("textEditor", "readonly");
+  const store = transaction.objectStore("textEditor");
+  const getAllRequest = store.getAll();
+  const result = await getAllRequest;
+  console.log("result.value", result);
+  return result;
+};
 
 initdb();
