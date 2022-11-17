8fbc59862eb6c8e65e621350cc963ee04a980145
miss-mad <mmlh.harris@gmail.com>
2022-11-17 17:25:04 -0500

    updated database and sw files

--- Develop/client/src/js/database.js
+++ Develop/client/src/js/database.js
@@ -16,15 +16,15 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => {
+export const putDb = async (id, content) => {
   console.error("putDb not implemented");
   const textEditorDb = await openDB("textEditor", 1);
   const transaction = textEditorDb.transaction("textEditor", "readwrite");
   const store = transaction.objectStore("textEditor");
   // insert data into put request
-  const putRequest = store.put({});
+  const putRequest = store.put({ id: 1, value: content });
   const result = await putRequest;
-  console.log("data updated in the database", result);
+  console.log("data updated in the database", result.value);
 };
 
 // TODO: Add logic for a method that gets all the content from the database
@@ -33,10 +33,11 @@ export const getDb = async () => {
   const textEditorDb = await openDB("textEditor", 1);
   const transaction = textEditorDb.transaction("textEditor", "readonly");
   const store = transaction.objectStore("textEditor");
-  const getAllRequest = store.getAll();
+  // const getAllRequest = store.getAll();
+  const getAllRequest = store.get(1);
   const result = await getAllRequest;
-  console.log("result.value", result);
-  return result;
+  console.log("result.value", result.value);
+  return result.value;
 };
 
 initdb();
